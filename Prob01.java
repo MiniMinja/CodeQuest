a4fc9d90eb3c23a21c4f04a55657b1ae089eb336
Minyoung Heo <minyoungheo.heo@gmail.com>
2020-01-25 09:32:16 -0800

    Created sample files

---
+++ Prob01.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Prob01{
+	public static void main(string[] args)throws IOException{
+		Scanner in = new Scanner(new File("Prob01.in.txt"));
+		PrintWriter out = new PrintWriter(new File("out.txt"));
+
+		int n = in.nextInt();
+		for(int i = 0;i < n;i++){
+
+		}
+
+		in.close();
+		out.close();
+		Check.check("out.txt", "Prob01.out.txt");
+	}
+}
